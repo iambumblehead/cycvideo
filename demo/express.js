7208241c452b646c6e24bc20635e7adf8c26f7a3
chris <chris@bumblehead.com>
2016-02-10 18:32:47 -0800

    removed babel stuff

--- demo/express.js
+++ demo/express.js
@@ -1,58 +1,85 @@
 // Filename: index.js  
-// Timestamp: 2016.02.10-15:40:05 (last modified)
+// Timestamp: 2016.02.10-18:29:51 (last modified)
 // Author(s): bumblehead <chris@bumblehead.com>
 
-import fs from 'fs';
-import express from 'express';
-import { run } from '@cycle/core';
-import { makeHTMLDriver } from '@cycle/dom';
+
+var express = require('express'),
+    morgan = require('morgan'),
+    favicon = require('serve-favicon'),
+    bodyParser = require('body-parser'),
+    serveIndex = require('serve-index'),
+    compression = require('compression'),
+    cookieParser = require('cookie-parser'),
+    errorhandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    scroungejs = require('scroungejs'),
+    http = require('http'),
+    app;
+
+var fs = require('fs');
+var cyclecore = require('@cycle/core');
+var cycledom = require('@cycle/dom');
+
+scroungejs.build({
+  inputpath      : './src/',
+  outputpath     : './www/',
+  publicpath     : './www',    
+  iscompressed   : false,
+  isconcatenated : true,
+  basepage       : './demo/index.html',  
+  treearr        : [
+    'cycvideo.js',
+    'cycvideo.css'
+  ]
+}, function (err, res) {
+  if (err) throw new Error(err);
+
+  console.log('build done?', err, res);
 
 // Error.stackTraceLimit = Infinity;
 const log = console.log,
-      production = process.env.NODE_ENV === 'production',
-      port       = process.env.PORT || 3000,
       app    = express(),
-      router = express.Router(),
-      mvisrc = require.resolve('../src/js/cycvideo'),
-      DOM    = makeHTMLDriver(),
+      mvisrc = require.resolve('../src/cycvideo'),
+      DOM    = cycledom.makeHTMLDriver(),
       main   = ({ DOM }) => ({ DOM: mvi(DOM) });
 
 var mvi = require(mvisrc);
 
-if (production) {
-  log('[pro]');
-  app.use(require('compression')());
-} else {
-  log('[dev]');
-  // use dev compilation and hot reloading
-  const config = require('./dev.babel').default,
-	compiler = require('webpack')(config),
-	dev = require('webpack-dev-middleware'),
-	hot = require('webpack-hot-middleware');
-
-  app.use(dev(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  })).use(hot(compiler));
-
-  // register mvi file with hot rebuilder
-  require('./hot').default({
-    [mvisrc]: next => { mvi = next; }
+  app.engine('html', function (fpath, data, fn) {
+    console.log('engine path');
+    
+    cyclecore.run(main, { DOM }).sources.DOM.forEach(ssr => {
+      fs.readFile('./demo/index.html', 'utf-8', function (err, content) {
+        res.end(content.replace(/__ssr__/, ssr));
+      });    
+    }, fn);
   });
-}
 
-router.get('/', (req, res, next) => {
-  run(main, { DOM }).sources.DOM.forEach(ssr => {
-    fs.readFile('./src/html/index.html', 'utf-8', function (err, content) {
-      res.end(content.replace(/__ssr__/, ssr));
-    });    
-  }, next);
-});
+  app.use(methodOverride());
+  app.use(compression());
 
-app.use(router).use(express.static('./public'));
+  app.use('/www', express.static(__dirname + '/../www/'));  
 
+  app.use('/', express.static(__dirname + '/'));
+  //app.use('/', serveIndex(__dirname + '/'));
+
+  // express' template path, path to index.html
+  //app.set('views', __dirname + '/');
+  //app.set('view engine', 'html');
+  //app.set('view options', {
+  //  layout : false
+  //});
+
+  app.use(errorhandler({
+    dumpExceptions : true, 
+    showStack : true
+  }));  
+  
 app.listen(port, 'localhost', err => {
   if (err) return console.err(err);
 
   log(`listening on http://localhost:${ port }`);
+});
+
+
 });
